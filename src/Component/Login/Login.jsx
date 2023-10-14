3b9d5c44a26fb3075b338fa3fcd2aac59320734e
Elvis Sunguti <elviskendrick99@gmail.com>
2023-10-14 16:37:55 +0300

    Added feature to the login and Sign up component

--- src/Component/Login/Login.jsx
+++ src/Component/Login/Login.jsx
@@ -1,33 +1,108 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import logo from "../../Assets/logo/logo-no-background.png";
+import { Images } from "../Utils/ArtData";
+import { Link } from "react-router-dom";
+
+
+function getRandomIndex(max) {
+    return Math.floor(Math.random() * max);
+  }
 
 const Login = () => {
+
+    const [ userName, setUserName ] = useState("")
+    const [ email, setEmail ] = useState("");
+    const [ password, setPassword ] = useState("");
+    const [ confirmPassword, setConfirmPassword ] = useState("");
+
+    const [firstImageIndex, setFirstImageIndex] = useState(getRandomIndex(Images.length));
+    const [secondImageIndex, setSecondImageIndex] = useState(getRandomIndex(Images.length));
+
+    useEffect(() => {
+        // Change the images randomly every 5 seconds (5000 milliseconds)
+        const interval = setInterval(() => {
+          setFirstImageIndex(getRandomIndex(Images.length));
+          setSecondImageIndex(getRandomIndex(Images.length));
+        }, 5000);
+    
+        return () => {
+          clearInterval(interval); // Cleanup the interval on unmount
+        };
+      }, []);
+
+
     return (
-        <section>
-            <div>
-                <div>
-                    <h1>Login</h1>
-                    <div>
-                        <form>
-                            <input
-                              type="text"
-                              name="email"
-                              placeholder="Enter email"
-                              required
-                              className=""
-                            />
-                            <input
-                              type="password"
-                              name="password"
-                              placeholder="Enter Password"
-                              required
-                              className="className"
-                            />
-                            <button type="submit">
-                                Login
-                            </button>
-                        </form>
-                    </div>
+        <section className="">
+            <div className="flex justify-center my-16">
+               <img 
+                  src={logo}
+                  alt="Logo image"
+                  className="h-32 w-96"
+                    />
+            </div>
+            <div className="flex flex-row ">
+                <div className="flex justify-end w-1/2">
+            <div className="flex flex-col justify-end w-1/2 mr-4">
+                    <img
+                       src={Images[firstImageIndex]}
+                       alt="Images of art"
+                       className="w-full h-48 rounded-md shadow-xl"
+
+                    />
+                    <img 
+                       src={Images[secondImageIndex]} 
+                       alt="Images of art"
+                       className="w-full h-48 rounded-md shadow-xl"
+                    />
                 </div>
+                </div>
+
+                <div className="flex w-1/2">
+                    <form className="flex flex-col ml-4 w-1/2 space-y-8 ">
+                        <h1 className="text-2xl font-semibold tracking-tight text-green-600">Login</h1>
+                        <div>
+                            <label htmlFor="email" className="flex flex-start block font-bold text-lg">
+                                Email
+                            </label>
+                        <input
+                           type="email"
+                           name="email"
+                           id="email"
+                           value={email}
+                           placeholder="Enter Email address"
+                           required
+                           onClick={(e) => setEmail(e.target.value)}
+                           className="block relative w-full px-3 py-2 rounded-none rounded-t-md border border-gray-300 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500"
+                        />
+                        </div>
+                        <div>
+                            <label htmlFor="password" className="flex flex-start block font-bold text-lg">
+                                Password
+                            </label>
+                        <input
+                           type="password"
+                           name="password"
+                           id="password"
+                           value={password}
+                           placeholder="Enter password"
+                           required
+                           onClick={(e) => setPassword(e.target.value)}
+                           className="block relative w-full px-3 py-2 rounded-none rounded-t-md border border-gray-300 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500"
+                        />
+                        </div>
+                        <div>
+                        <button type="submit" className="px-2 py-3 bg-green-500 hover:bg-green-700 text-lg text-white shadow-xl rounded">
+                            Login
+                        </button>
+                        </div>
+                        <div className="mb-8">
+                        <p>Don't have an account? <Link to="/sign up"><span className="text-red-500">Sign up</span></Link>
+
+                        </p>
+                        </div>
+                    </form>
+                </div>
+
             </div>
         </section>
     )
--- src/Component/SignUp/SignUp.jsx
+++ src/Component/SignUp/SignUp.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import logo from "../../Assets/logo/logo-no-background.png";
 import { Images } from "../Utils/ArtData";
+import { Link } from "react-router-dom";
 
 
 function getRandomIndex(max) {
@@ -8,6 +9,11 @@ function getRandomIndex(max) {
   }
 
 const SignUp = () => {
+
+    const [ userName, setUserName ] = useState("")
+    const [ email, setEmail ] = useState("");
+    const [ password, setPassword ] = useState("");
+    const [ confirmPassword, setConfirmPassword ] = useState("");
 
     const [firstImageIndex, setFirstImageIndex] = useState(getRandomIndex(Images.length));
     const [secondImageIndex, setSecondImageIndex] = useState(getRandomIndex(Images.length));
@@ -26,60 +32,100 @@ const SignUp = () => {
 
 
     return (
-        <section>
+        <section className="">
+            <div className="flex justify-center my-16">
                <img 
                   src={logo}
                   alt="Logo image"
-                  className=""
+                  className="h-32 w-96"
                     />
-            <div className="flex flex-col">
+            </div>
+            <div className="flex flex-row">
 
-                <div className="flex flex-col w-1/2">
-                    <h1>Sign Up</h1>
-                    <form className="flex flex-col ">
+                <div className="flex justify-end w-1/2">
+                    <form className="flex flex-col justify-end mr-4 w-1/2 space-y-8 ">
+                        <h1 className="text-2xl font-semibold tracking-tight text-green-600">Sign Up</h1>
+                        <div>
+                          <label htmlFor="userName" className="flex flex-start block font-bold text-lg">
+                            Username
+                          </label>  
                         <input 
                            type="name"
                            name="name"
-                           placeholder="Enter Full name"
+                           id="userName"
+                           value={userName}
+                           placeholder="Enter username"
                            required
-                           className=""
+                           onClick={(e) => setUserName(e.target.value)}
+                           className="block relative w-full px-3 py-2 rounded-none rounded-t-md border border-gray-300 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500"
                         />
+                        </div>
+                        <div>
+                            <label htmlFor="email" className="flex flex-start block font-bold text-lg">
+                                Enter
+                            </label>
                         <input
                            type="email"
                            name="email"
+                           id="email"
+                           value={email}
                            placeholder="Enter Email address"
                            required
-                           className=""
+                           onClick={(e) => setEmail(e.target.value)}
+                           className="block relative w-full px-3 py-2 rounded-none rounded-t-md border border-gray-300 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500"
                         />
+                        </div>
+                        <div>
+                            <label htmlFor="password" className="flex flex-start block font-bold text-lg">
+                                Password
+                            </label>
                         <input
                            type="password"
                            name="password"
+                           id="password"
+                           value={password}
                            placeholder="Enter password"
                            required
-                           className=""
+                           onClick={(e) => setPassword(e.target.value)}
+                           className="block relative w-full px-3 py-2 rounded-none rounded-t-md border border-gray-300 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500"
                         />
+                        </div>
+                        <div>
+                            <label htmlFor="confirmPassword" className="flex flex-start block font-bold text-lg">
+                                Confirm password
+                            </label>
                         <input 
                            type="password"
                            name="password"
+                           id="confirmPassword"
+                           value={confirmPassword}
                            required
                            placeholder="Confirm password"
-                           className=""
+                           onClick={(e) => setConfirmPassword(e.target.value)}
+                           className="block relative w-full px-3 py-2 rounded-none rounded-t-md border border-gray-300 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500"
                         />
-                        <button type="submit" className="bg-green-500 text-white">
+                        </div>
+                        <div>
+                        <button type="submit" className="px-2 py-3 bg-green-500 hover:bg-green-700 text-white shadow-xl rounded">
                             Sign up
-                        </button >
-                        <p>Already have an account? Login here</p>
+                        </button>
+                        </div>
+                        <div className="mb-8">
+                        <p>Already have an account? <Link to="/login"><span className="text-red-500">Login</span></Link></p>
+                        </div>
                     </form>
                 </div>
-                <div>
+                <div className="w-1/2 ml-4">
                     <img
                        src={Images[firstImageIndex]}
                        alt="Images of art"
+                       className="w-1/2 h-64 rounded-md shadow-xl"
 
                     />
                     <img 
                        src={Images[secondImageIndex]} 
                        alt="Images of art"
+                       className="w-1/2 h-64 rounded-md shadow-xl"
                     />
                 </div>
             </div>
