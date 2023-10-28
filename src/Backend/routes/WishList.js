4cab0086f1521d57c203318e32011e47db1bf178
Elvis Sunguti <elviskendrick99@gmail.com>
2023-10-28 05:44:28 +0300

    Added BE code to delete artwork from wishlist

--- src/Backend/routes/WishList.js
+++ src/Backend/routes/WishList.js
@@ -31,4 +31,36 @@ async (req, res) => {
 });
 
 
+router.post("/deletewishlist/:artWorkId",
+passport.authenticate("jwt", {session: false}),
+async(req, res) => {
+    try{
+
+        const userId = req.user._id;
+
+        const artWorkId = req.params.artWorkId;
+
+        const user = await User.findById(userId);
+
+        if (!user){
+            return res.json({ error: "User npt found"})
+        };
+
+        const index = user.wishList.indexOf(artWorkId);
+        if (index !== -1) {
+          // Remove the artwork ID from the wishlist
+          user.wishList.splice(index, 1);
+          await user.save();
+  
+          return res.json({ message: "Artwork removed from wishlist" });
+        } else {
+          return res.status(404).json({ error: "Artwork not found in wishlist" });
+        }
+
+    } catch (error){
+        console.error("Error removing artwork from wishList", error);
+        return res.json({ Error: "Error removing artwork from wishlist"});
+    }
+});
+
 module.exports = router;
