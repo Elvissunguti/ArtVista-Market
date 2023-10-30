12de2b220cb963461098ee775ea4e61dee61cb1a
Elvis Sunguti <elviskendrick99@gmail.com>
2023-10-30 16:36:44 +0300

    Added BE code to remove artwork from cartList

--- src/Backend/routes/CartList.js
+++ src/Backend/routes/CartList.js
@@ -29,4 +29,33 @@ async(req, res) => {
     };
 });
 
+
+router.post("/deletecartlist/:artWorkId",
+passport.authenticate("jwt", { session: false}),
+async (req, res) => {
+    try{
+
+        const userId = req.user._id;
+
+        const artWorkId = req.params.artWorkId;
+
+        const user = await User.findById(userId);
+
+        const index = user.cartList.indexOf(artWorkId);
+        if (index !== -1){
+            user.cartList.splice(index, 1);
+            user.cartListNumber = user.cartList.length;
+            await user.save();
+
+            return res.json({ message: "Artwork removed from cartlist" });
+        } else {
+            return res.json({ error: "Artwork not found in CartList" })
+        }
+
+    } catch (error) {
+        console.error("Error deleting artwork from cartList", error);
+        return res.json({ error: "Error deleting artwork from cartList" });
+    }
+})
+
 module.exports = router;
