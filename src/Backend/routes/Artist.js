e7a5d84f3270797264d33f09c6426e766bd09e2c
Elvis Sunguti <elviskendrick99@gmail.com>
2023-11-08 15:11:55 +0300

    Added BE code to fetch artist details

--- src/Backend/routes/Artist.js
+++ src/Backend/routes/Artist.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const passport = require("passport");
 const User = require("../Model/User");
 const ArtWork = require("../Model/ArtWork");
+const Profile = require("../Model/Profile");
 
 
 router.get("/allartist",
@@ -28,6 +29,35 @@ async (req, res) => {
     } catch (error){
         console.error("Error fetching the name of all the artist", error);
         return res.json({ error: "Error fetcing the name of all the artist"})
+    }
+})
+
+router.get("/get/artist",
+passport.authenticate("jwt", {session: false}),
+async (req, res) => {
+    try{
+
+        const artist = await User.find({}, 'userName');
+
+        const artistInfo = await Promise.all(artist.map( async (artist) => {
+            const artistId = artist._id;
+          
+            const profile = await Profile.findOne({ userId: artistId });
+
+            const profilePic = profile ? profile.profilePic : null;
+
+            return {
+                userName: artist.userName,
+                profilePic: profilePic.replace("../../../public", ""),
+                artistId,
+            }
+        }))
+        
+        return res.json({ data: artistInfo })
+
+    } catch (error){
+        console.error("Error fetching artist details", error);
+        return res.json({ error: "Error fetching the details of the artist"});
     }
 })
 
