ac9ab6f7286652bebae0905e1c7f1663a79c5315
Elvis Sunguti <elviskendrick99@gmail.com>
2023-11-08 05:49:10 +0300

    Changed how the profile is created so that one can also be able to update it

--- src/Backend/routes/Profile.js
+++ src/Backend/routes/Profile.js
@@ -4,34 +4,43 @@ const Profile = require("../Model/Profile");
 const passport = require("passport");
 const router = express.Router();
 
-router.post("/create",
-passport.authenticate("jwt", {session: false}),
-(req, res) => {
+router.post("/create", passport.authenticate("jwt", { session: false }), async (req, res) => {
     profilePicUploads(req, res, async (err) => {
-        if(err){
+        if (err) {
             return res.json({ err: "Failed to upload files" });
-        };
+        }
 
-    try{
-        const { description, location } = req.body;
+        try {
+            const { description, location } = req.body;
+            const profilePic = req.files["profilePic"][0].path;
+            const userId = req.user._id;
 
-        const profilePic = req.files["profilePic"][0].path;
-        const userId = req.user._id;
+            // Check if a profile already exists for the user
+            const existingProfile = await Profile.findOne({ userId });
 
-        const newProfile = new Profile({
-            description,
-            location,
-            profilePic,
-            userId
-        });
-
-        await newProfile.save();
-        return res.json({ message: "Profile created successfully" })
-    } catch (error){
-        console.error("Error creating user profile", error)
-        return res.json({ Error: "Error creating user profile"})
-    }
-})
-}); 
+            if (existingProfile) {
+                // If a profile exists, update it
+                existingProfile.description = description;
+                existingProfile.location = location;
+                existingProfile.profilePic = profilePic;
+                await existingProfile.save();
+                return res.json({ message: "Profile updated successfully" });
+            } else {
+                // If no profile exists, create a new one
+                const newProfile = new Profile({
+                    description,
+                    location,
+                    profilePic,
+                    userId
+                });
+                await newProfile.save();
+                return res.json({ message: "Profile created successfully" });
+            }
+        } catch (error) {
+            console.error("Error creating or updating user profile", error);
+            return res.json({ Error: "Error creating or updating user profile" });
+        }
+    });
+});
 
 module.exports = router;
