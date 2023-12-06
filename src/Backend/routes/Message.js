cbdf217efb8e08028ed38d3021e58291d44b9c65
Elvis Sunguti <elviskendrick99@gmail.com>
2023-12-06 11:02:37 +0300

    Added webSocket library to make sending and reading of messages more easy

--- src/Backend/routes/Message.js
+++ src/Backend/routes/Message.js
@@ -2,20 +2,48 @@ const express = require("express");
 const router = express.Router();
 const Message = require("../Model/Message");
 const passport = require("passport");
+const { WebSocketServer } = require("ws");
+
+const wss = new WebSocketServer({ port: 8081 });
+
+const clients = new Map();
+
+// Handle WebSocket connections
+wss.on("connection", (ws, req) => {
+    const userId = req.user._id;
+    clients.set(userId, ws);
+  
+    ws.on("close", () => {
+      clients.delete(userId);
+    });
+  });
 
 
-router.post("/create",
+
+router.post("/create/:artistId",
 passport.authenticate("jwt", {session: false}),
 async (req, res) => {
     try{
         const { content } = req.body;
         const userId = req.user._id;
+        const artistId = req.params.artistId;
 
-        const newMessage = new Message({ content, userId});
+        const newMessage = new Message({ content, userId, artistId});
 
         await newMessage.save();
 
-        res.json({ newMessage});
+        const artistSocket = clients.get(artistId); // Assuming artistId is unique
+        const userSocket = clients.get(userId);
+
+        if (artistSocket) {
+            artistSocket.send(JSON.stringify({ newMessage }));
+        }
+
+        if (userSocket) {
+            userSocket.send(JSON.stringify({ newMessage }));
+        }
+
+        res.json({ newMessage });
 
 
     } catch (error) {
